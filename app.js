340cd637c8312b7efa1869f935e9d77abc0d878a
Taleb_84 <taleb_n1@hotmail.com>
2019-11-21 00:56:06 +0100

    create index1.html
    Read an exesting file with nodejs

--- app.js
+++ app.js
@@ -73,8 +73,18 @@
 // }
 // checkSpace("hello");
 
+// let http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('This is my first server with node.js');
+// }).listen(8080);
+
 let http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('This is my first server with node.js');
-}).listen(8080);
+let fs = require('fs');
+http.createServer( function (req, res) {
+  fs.readFile('index1.html', function (err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080)
