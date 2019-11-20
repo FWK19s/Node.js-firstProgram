c5648f9da077260a8514fc06d5c5659ff6babfa6
Taleb_84 <taleb_n1@hotmail.com>
2019-11-21 00:43:43 +0100

    create server
    create server with node.js and show the content as html on port 8080

--- app.js
+++ app.js
@@ -1,70 +1,80 @@
-function sayHello(name) {
-    console.log('Hello ' + name);
-}
-sayHello('Oscar');
+// function sayHello(name) {
+//     console.log('Hello ' + name);
+// }
+// sayHello('Oscar');
 
-let x = 10;
-let y = 10;
-console.log(x + y);
+// let x = 10;
+// let y = 10;
+// console.log(x + y);
 
-function hello(x) {
-    console.log(x);
-}
-hello("HelloWorld");
+// function hello(x) {
+//     console.log(x);
+// }
+// hello("HelloWorld");
 
-function greeting(name) {
-    console.log("Hello " + name)
-}
-greeting("Oscar");
+// function greeting(name) {
+//     console.log("Hello " + name)
+// }
+// greeting("Oscar");
 
-function secounds (x) {
-    console.log(x * 24 * 60 * 60);
-}
-secounds(1);
+// function secounds (x) {
+//     console.log(x * 24 * 60 * 60);
+// }
+// secounds(1);
 
-function getFirstElement() {
-    let arr = [59,79,44];
-    console.log(arr[0]);
-}
-getFirstElement();
+// function getFirstElement() {
+//     let arr = [59,79,44];
+//     console.log(arr[0]);
+// }
+// getFirstElement();
 
-function sum(x, y) {
-    console.log(x + y);
-}
-sum(4,7);
+// function sum(x, y) {
+//     console.log(x + y);
+// }
+// sum(4,7);
 
-function div(x) {
-    console.log(x % 3 === 0);
-}
-div(6);
+// function div(x) {
+//     console.log(x % 3 === 0);
+// }
+// div(6);
 
-function str(x) {
-    console.log(x.length);
-}
-str("Hello friend I am learning nodejs");
+// function str(x) {
+//     console.log(x.length);
+// }
+// str("Hello friend I am learning nodejs");
 
-function arrReverse (x) {
-    console.log(x.reverse());
-}
-arrReverse([4,77,68]);
+// function arrReverse (x) {
+//     console.log(x.reverse());
+// }
+// arrReverse([4,77,68]);
 
-function lastItem(x) {
-    console.log(x.pop());
-}
-lastItem([4,76,8,33,12]);
+// function lastItem(x) {
+//     console.log(x.pop());
+// }
+// lastItem([4,76,8,33,12]);
 
-function num(x) {
-    if (x % 2 === 0) {
-        console.log("even");
-    } else {
-        console.log("odd");
-    }
-}
-num(56);
-
-function checkSpace(str) {
-    console.log((/\s/).test(str));
-}
-checkSpace("hello");
+// function num(x) {
+//     if (x % 3 === 0) {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
+// }
+// num(56);
 
 
+
+
+
+
+// function checkSpace(str) {
+//     reg = /\s/;
+//     console.log(reg.test(str));
+// }
+// checkSpace("hello");
+
+let http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('This is my first server with node.js');
+}).listen(8080);
